1f575e9819c60ff46493aca99aa7a5833475ffa3
Марат Суяргулов <suyargulovml@sber-service.ru>
2024-09-03 23:15:53 +0300

    Lesson2

---
+++ lesson_002.py
@@ -0,0 +1,8 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = "Примитивно"
+print (example[0])
+print (example[-1])
+print (example[5:])
+print (example[::-1])
+print (example[1::2])
