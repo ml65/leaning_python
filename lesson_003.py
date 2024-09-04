9b60295eb7a9e6311a03fcc6ce76bc8283f4d7f0
Марат Суяргулов <suyargulovml@sber-service.ru>
2024-09-04 13:46:48 +0300

    Практическое задание по теме "Переменные".

---
+++ lesson_003.py
@@ -0,0 +1,15 @@
+# 2023/09/21 00:00|Практическое задание по теме "Переменные".
+
+# snake_case
+home_work_count = 12
+period = 1.5
+course_name = 'Python'
+home_work_time = period / home_work_count
+print ('Курс: ', course_name, ', всего задач: ', home_work_count, ', затрачено часов: ', period, ', среднее время выполнения ',home_work_time,' часа.')
+
+# camelCase
+homeWorkCount = 12
+period = 1.5
+courseName = 'Python'
+homeWorkTime = period / home_work_count
+print ('Курс: ', courseName, ', всего задач: ', homeWorkCount, ', затрачено часов: ', period, ', среднее время выполнения ',homeWorkTime,' часа.')
