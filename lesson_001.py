49ccc29886879457cb2ee942bf0e343f7c2caa9c
Марат Суяргулов <suyargulovml@sber-service.ru>
2024-09-04 13:54:01 +0300

    Правка lesson_001

--- lesson_001.py
+++ lesson_001.py
@@ -2,8 +2,7 @@
 print ( 9 ** 0.5 * 5)
 
 # 2nd program
-if (9.99 > 9.98 and 1000 != 1000.1):
-    print (.99 > 9.98 and 1000 != 1000.1)
+print (9.99 > 9.98 and 1000 != 1000.1)
 
 # 3nd program
 print (2 * 2 + 2)
@@ -11,4 +10,4 @@ print (2 * ( 2 + 2 ))
 print ((2 * 2 + 2) == (2 * ( 2 + 2 )))
 
 # 4th program
-print (int(float('123.456')*10) - int(float('123.456')) * 10)
+print (int(float('123.456')*10) % 10)
