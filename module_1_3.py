8b8333e44be4c4885c9aa2104f19ed46a26fb62c
Марат Суяргулов <suyargulovml@sber-service.ru>
2024-09-05 08:58:24 +0300

    module_1_3

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+# Практическая работа по уроку "Динамическая типизация"
+
+name = "Марат"
+print (name)
+
+age = 59
+print (age)
+
+age += 12 #age = age + 12
+print (age)
+
+is_student = True
+print (is_student)
