32cbb74cf97979271d6821840b07a62d70e157ca
Марат Суяргулов <suyargulovml@sber-service.ru>
2024-09-06 23:38:36 +0300

    module_1_6_dop

--- module_1_6_dop.py
+++ module_1_6_dop.py
@@ -2,8 +2,20 @@
 
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-print (type(students))
-print (sorted(students))
+
+# Решение без циклов
+grades_m = [sum(grades[0])/len(grades[0]),
+            sum(grades[1])/len(grades[1]),
+            sum(grades[2])/len(grades[2]),
+            sum(grades[3])/len(grades[3]),
+            sum(grades[4])/len(grades[4])]
+
+students_sort = sorted(students)
+
+dict1 = dict(zip(students_sort, grades_m))
+print (dict1)
+
+# Решение с циклами
 ptr = 0
 gratePointAverage = {}
 for name in sorted(students):
