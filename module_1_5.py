3ee13d59b281f08c0bd08f22f7de3f0e8e27a1bb
Марат Суяргулов <suyargulovml@sber-service.ru>
2024-09-06 14:18:54 +0300

    module_1_5

---
+++ module_1_5.py
@@ -0,0 +1,13 @@
+# Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"
+
+immutable_var = 1, 2, "три", 4.0, True
+print (immutable_var)
+
+# immutable_var[0] = False
+# print (immutable_var)
+
+mutable_list = [ 5, 4, "три", 2.0, False]
+mutable_list[2] = 3
+print(mutable_list)
+
+
