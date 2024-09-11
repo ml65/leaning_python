703fbca21cfe9a0bf15a3be0cd2ae3278d64d0cd
Марат Суяргулов <suyargulovml@sber-service.ru>
2024-09-11 06:03:19 +0300

    module_2_hard

--- module_2_hard.py
+++ module_2_hard.py
@@ -10,6 +10,12 @@ def indiana_password(num):
                     result  += str(i) + str(j)
     return result
 
+print ("Вы перед воротами введите число с первой вставки.")
 while 1 > 0:
+    print ("Для выхода введите 0")
     number = int(input("Введите число: "))
+    if 0 == number:
+        break
     print ("Пароль:", indiana_password(number))
+
+print ("Приключение закончилось!")
