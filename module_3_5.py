94891d88b6bc2bdf79c2affc7d45ba88349ef476
Марат Суяргулов <suyargulovml@sber-service.ru>
2024-09-17 00:41:45 +0300

    module_3_5_bagfix

--- module_3_5.py
+++ module_3_5.py
@@ -8,7 +8,10 @@ def get_multiplied_digits(number):
     if (len(str_number) > 1):
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return first
+        if first > 0:
+            return first
+        else:
+            return 1
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(420)
 print (result)
