aed20e0fb6fa2b741d983180fe8b84a252a6412d
Marat Suyargulov <ml65@rambler.ru>
2024-09-22 23:06:28 +0300

    module_5_3

--- module_5_3.py
+++ module_5_3.py
@@ -23,13 +23,94 @@ class House:
     def __str__(self):
         return f"Название {self.name}, кол-во этажей: {self.number_of_floors}"
 
+    def __eq__(self, other):
+        if (other.__class__.__name__ == "int"):
+            return self.number_of_floors == other
+        elif (other.__class__.__name__ == "House"):
+            return self.number_of_floors == other.number_of_floors
+        # исключение - пока не изучали
+        print(f"Недопустимый параметр класса {other.__class__.__name__}" )
+        return False
+
+    def __lt__(self, other):
+        if (other.__class__.__name__ == "int"):
+            return self.number_of_floors < other
+        elif (other.__class__.__name__ == "House"):
+            return self.number_of_floors < other.number_of_floors
+        # исключение - пока не изучали
+        print(f"Недопустимый параметр класса {other.__class__.__name__}" )
+        return False
+
+    def __le__(self, other):
+        if (other.__class__.__name__ == "int"):
+            return self.number_of_floors <= other
+        elif (other.__class__.__name__ == "House"):
+            return self.number_of_floors <= other.number_of_floors
+        # исключение - пока не изучали
+        print(f"Недопустимый параметр класса {other.__class__.__name__}" )
+        return False
+
+    def __gt__(self, other):
+        if (other.__class__.__name__ == "int"):
+            return self.number_of_floors > other
+        elif (other.__class__.__name__ == "House"):
+            return self.number_of_floors > other.number_of_floors
+        # исключение - пока не изучали
+        print(f"Недопустимый параметр класса {other.__class__.__name__}" )
+        return False
+
+    def __ge__(self, other):
+        if (other.__class__.__name__ == "int"):
+            return self.number_of_floors >= other
+        elif (other.__class__.__name__ == "House"):
+            return self.number_of_floors >= other.number_of_floors
+        # исключение - пока не изучали
+        print(f"Недопустимый параметр класса {other.__class__.__name__}" )
+        return False
+
+    def __ne__(self, other):
+        if (other.__class__.__name__ == "int"):
+            return self.number_of_floors != other
+        elif (other.__class__.__name__ == "House"):
+            return self.number_of_floors != other.number_of_floors
+        # исключение - пока не изучали
+        print(f"Недопустимый параметр класса {other.__class__.__name__}" )
+        return False
+
+    def __add__(self, value):
+        if (value.__class__.__name__ == "int"):
+            self.number_of_floors += value
+            return self
+        # исключение - пока не изучали
+        print(f"Недопустимый параметр класса {value.__class__.__name__}" )
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
 print(h1)
 print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
+print(h1 == h2)  # __eq__
+
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10  # __iadd__
+print(h1)
+
+h2 = 10 + h2  # __radd__
+print(h2)
+
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
