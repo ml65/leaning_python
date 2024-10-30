b8b5f05cdd44d4b2615f8f41a9cdacf61cad7f49
Marat Suyargulov <ml65@rambler.ru>
2024-10-30 11:38:42 +0300

    module_9_6

---
+++ module_9/module_9_6.py
@@ -0,0 +1,20 @@
+# Домашнее задание по теме "Генераторы"
+
+#
+
+def all_variants(text):
+    for width in range(1,len(text)+1):
+        begin = 0
+        while (begin + width <= len(text)):
+            resp = ''
+            for j in range(begin, begin + width):
+                resp += text[j]
+            begin += 1
+            yield resp
+
+
+text = "abc"
+print(f"begin={text[0]}")
+a = all_variants("abc")
+for i in a:
+    print(i)
