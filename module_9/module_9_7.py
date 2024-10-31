750e9171f3561b92d37ec7ab4f9510ecc4f558d7
Marat Suyargulov <ml65@rambler.ru>
2024-10-31 18:55:39 +0300

    module_9_7

--- module_9/module_9_7.py
+++ module_9/module_9_7.py
@@ -4,27 +4,42 @@
 def is_prime(func):
     def wrapper(*args):
         result = func(*args)
-        if isinstance(result, bool):
-            print("ERROR!")
-        elif isinstance(result, int):
-            print("Простое")
-        else:
-            print("Составное")
+        if (result):
+            is_prime = True
+
+            if result <= 1:
+                is_prime = False
+            if result == 2:
+                is_prime = True
+            if result % 2 == 0:
+                is_prime = False
+            for i in range(3, int(result ** 0.5) + 1, 2):
+                if result % i == 0:
+                    is_prime = False
+            if is_prime:
+                print("Простое")
+            else:
+                print("Составное")
+
         return result
     return wrapper
 
 @is_prime
-def sum_three(first, second, third):
+def sum_three(first:int, second:int, third:int):
     try:
-        result = first + second + third
+        result = int(first) + int(second) + int(third)
     except TypeError:
-        result = False
+        print("ERROR!")
+        result = ""
+    except ValueError:
+        print("ERROR!!!")
+        result = ""
     return result
 
 
 
 
-result = sum_three(2, 3, 4)
+result = sum_three(12, 3, 4)
 print(result)
 print()
 result = sum_three(2, 3, 7.1)
