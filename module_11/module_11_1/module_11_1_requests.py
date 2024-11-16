6be5b7221f7599e9514c9ab9e5e71e5d62a3c53c
Marat Suyargulov <ml65@rambler.ru>
2024-11-16 18:52:01 +0300

    module_11_1

--- module_11/module_11_1/module_11_1_requests.py
+++ module_11/module_11_1/module_11_1_requests.py
@@ -1,43 +1,18 @@
+# Домашнее задание по теме "Обзор сторонних библиотек Python"
+
+# requests - запросить данные с сайта и вывести их в консоль.
+
 import requests
-from requests.auth import HTTPBasicAuth
-
-
-def print_headers(response):
-    if (response.status_code == 200):
-        # обрабатываем полученные данные
-        for key in response.headers:
-            print(key, "=", response.headers[key])
-    else:
-        print('ERROR!', response.status_code, )
-
-
+import re
 # GET
-#print("====== GET =======\n")
-response = requests.get('https://google.com')
-#print_headers(response)
-print(response.content)
+url = 'https://lipsum.com/'
+response = requests.get(url)
 
-'''
-# POST
-print("====== POST =======\n")
-url = 'https://www.w3schools.com/python/demopage.php'
-myobj = {'somekey': 'somevalue'}
+# вывод текста
+res = re.sub(r"<[^>]+>", "", response.text, flags=re.S)
+print(res)
 
-response = requests.post(url, json = myobj)
-print_headers(response)
-
-# PUT
-print("====== PUT =======\n")
-
-response = requests.put('http://example.com/api/resource',
-                        json={'key': 'value'},
-                        auth=HTTPBasicAuth('username', 'password'))
-print_headers(response)
-print(response.status_code)  # Если ответ не 200, значит возникли проблемы
+# вывод html
+#print(response.content)
 
 
-# DELETE
-print("====== DELETE =======\n")
-response = requests.delete('https://books.ru/1231231')
-print_headers(response)
-'''
--- module_11/module_11_1/module_11_1_requests_test.py
+++ module_11/module_11_1/module_11_1_requests_test.py
@@ -1,16 +1,45 @@
-# Домашнее задание по теме "Обзор сторонних библиотек Python"
 
-# requests - запросить данные с сайта и вывести их в консоль.
 
 import requests
-import re
+from requests.auth import HTTPBasicAuth
+
+
+def print_headers(response):
+    if (response.status_code == 200):
+        # обрабатываем полученные данные
+        for key in response.headers:
+            print(key, "=", response.headers[key])
+    else:
+        print('ERROR!', response.status_code, )
+
+
 # GET
-url = 'https://lipsum.com/'
-response = requests.get(url)
+#print("====== GET =======\n")
+response = requests.get('https://google.com')
+#print_headers(response)
+print(response.content)
 
-# вывод текста
-res = re.sub(r"<[^>]+>", "", response.text, flags=re.S)
-print(res)
+'''
+# POST
+print("====== POST =======\n")
+url = 'https://www.w3schools.com/python/demopage.php'
+myobj = {'somekey': 'somevalue'}
 
-# вывод html
-#print(response.content)
+response = requests.post(url, json = myobj)
+print_headers(response)
+
+# PUT
+print("====== PUT =======\n")
+
+response = requests.put('http://example.com/api/resource',
+                        json={'key': 'value'},
+                        auth=HTTPBasicAuth('username', 'password'))
+print_headers(response)
+print(response.status_code)  # Если ответ не 200, значит возникли проблемы
+
+
+# DELETE
+print("====== DELETE =======\n")
+response = requests.delete('https://books.ru/1231231')
+print_headers(response)
+'''
